e09c07fc34bb4a0cc96a5726bc51dff5f25f8092
Fuzzy-Fluffy-Cat <christianmir37@gmail.com>
2024-03-01 10:10:37 +0800

    changed product image URL to localhost

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -72,7 +72,7 @@ const inventoryController = {
                     type: product.type,
                     quantity: product.quantity,
                     price: product.price,
-                    productpic: 'https://pearl-hardware-ph.onrender.com/image/' + imageSave._id,
+                    productpic: /* 'https://pearl-hardware-ph.onrender.com/image/' */ 'https://localhost:3000/image/' + imageSave._id,
                     description: product.description
                 }).save();
 
@@ -116,7 +116,7 @@ const inventoryController = {
                         type: product.type,
                         quantity: product.stock,
                         price: product.price,
-                        productpic: 'https://pearl-hardware-ph.onrender.com/image/' + imageSave._id,
+                        productpic: /*'https://pearl-hardware-ph.onrender.com/image/'*/ 'https://localhost:3000/image/' + imageSave._id,
                         description: product.description
                     },
                     

e7b753ff7fac4a2a080f2189218e52f9a0cc2ff0
Fuzzy-Fluffy-Cat <christianmir37@gmail.com>
2024-03-07 22:33:14 +0800

    Added email verification controller (hbs rendering only)
    Added email verification handlebars to call partial

---
+++ controllers/emailVerificationController.js
@@ -0,0 +1,22 @@
+/*
+*   This is the controller for email verification of a new user
+*/
+
+import exp from 'constants';
+import database from '../model/db.js';
+import { User } from '../model/userSchema.js';
+
+const emailVerificationController = {
+	getEmailVerify: async function (req, res) {
+        try {
+            res.render("emailVerify", {
+                //change script here; did not change it for now
+                script: './js/login.js'
+            });
+        } catch {
+            res.sendStatus(400);
+        }
+    }
+}
+
+export default emailVerificationController;

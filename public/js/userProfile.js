b75b2414860677ff4eb32174af0e1c33eac5e591
nathan-andrei <nathan_andrei_asnan@dlsu.edu.ph>
2024-03-14 11:58:14 +0800

    Added event listener to verify button to redirect to verify page with extra skipped flag

--- public/js/userProfile.js
+++ public/js/userProfile.js
@@ -1,7 +1,12 @@
 const title = document.querySelector('#title');
+const verBtn = document.querySelector('.verify-button');
 
 title.addEventListener('click', async (e) => {
     e.preventDefault();
 
     window.location.href = '/';
+})
+
+verBtn.addEventListener('click', async() => {
+	window.location.href = '/emailVerify?sk=true';
 })

cf79226bffec7c7dc3dc4cfc1cdc9954bc0fa1d9
Matthew Chua <matthew_adrian_u_chua@dlsu.edu.ph>
2024-03-17 21:19:00 +0800

    update loginController test file

--- tests/loginController.test.js
+++ tests/loginController.test.js
@@ -1,5 +1,6 @@
 import loginController from '../controllers/loginController.js';
 import { User } from '../model/userSchema.js';
+import bcrypt from 'bcrypt';
 import {jest} from '@jest/globals'
 import { describe } from 'node:test';
 
@@ -28,6 +29,54 @@ describe('Login Controller', () => {
 
   afterEach(() => {
     jest.clearAllMocks();
+  });
+
+  it('should return status 200 for valid credentials', async () => {
+    // Mock existing user with valid credentials
+    const existingUser = { //This is a dummy user that will be used as the comparison in the login function later when its called
+      _id: 'someUserId',
+      firstName: 'John',
+      email: 'test12345@email.com',
+      password: await bcrypt.hash('12345', 10),
+      isAuthorized: false
+    };
+    jest.spyOn(User, 'findOne').mockResolvedValue(existingUser); //this code sends the existingUser dummy data to the login function and acts as the user data in the database when logging in
+
+    await loginController.login(req, res); //calls the function to be tested with the existingUser dummy data as the user being logged in
+
+    expect(res.sendStatus).toHaveBeenCalledWith(200); //expects to return 200 for success
+  });
+
+  it('should return status 201 for valid credentials for admin account', async () => {
+    // Mock existing user with valid credentials
+    const existingUser = { //This is a dummy user that will be used as the comparison in the login function later when its called
+      _id: 'someAdminId',
+      firstName: 'John',
+      email: 'test12345@email.com',
+      password: await bcrypt.hash('12345', 10),
+      isAuthorized: true
+    };
+    jest.spyOn(User, 'findOne').mockResolvedValue(existingUser); //this code sends the existingUser dummy data to the login function and acts as the user data in the database when logging in
+
+    await loginController.login(req, res); //calls the function to be tested with the existingUser dummy data as the user being logged in
+
+    expect(res.sendStatus).toHaveBeenCalledWith(201); //expects to return 200 for success
+  });
+
+  it('should return status 500 for invalid password', async () => {
+    // Mock existing user with valid credentials
+    const existingUser = { //This is a dummy user that will be used as the comparison in the login function later when its called
+      _id: 'someAdminId',
+      firstName: 'John',
+      email: 'test12345@email.com',
+      password: await bcrypt.hash('123456', 10), //notice here the password should be 123456, while in the testcase its trying to login using password 12345 in the req
+      isAuthorized: false
+    };
+    jest.spyOn(User, 'findOne').mockResolvedValue(existingUser); //this code sends the existingUser dummy data to the login function and acts as the user data in the database when logging in
+
+    await loginController.login(req, res); //calls the function to be tested with the existingUser dummy data as the user being logged in
+
+    expect(res.sendStatus).toHaveBeenCalledWith(500); //expects to return 500
   });
 
   it('should return status 500 for invalid credentials or email', async () => {

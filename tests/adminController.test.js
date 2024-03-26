da3e07b7885ba98046a9afc8f93ae1fc647cfaa4
Kate Rojo <kate_rojo@dlsu.edu.ph>
2024-03-26 16:23:40 +0800

    emailVerificationController testscript (getEmailVerify only)

--- tests/adminController.test.js
+++ tests/adminController.test.js
@@ -1,3 +1,4 @@
+import { describe } from 'node:test';
 import adminController from '../controllers/adminController';
 import { User } from '../model/userSchema';
 import { jest } from '@jest/globals';
@@ -9,72 +10,74 @@ jest.mock('../model/userSchema.js', () => ({
 }));
 
 describe('adminController', () => {
-    let req, res;
+    describe('getAdmin', () => {
+        let req, res;
 
-    beforeEach(() => {
-        req = {
-            session: {
-                userID: 'mockUserID',
-            }
-        };
-        res = {
-            render: jest.fn(),
-            sendStatus: jest.fn(),
-        };
-    });
-
-    afterEach(() => {
-        jest.clearAllMocks();
-    });
-
-    it('should render adminHome if user is authorized', async () => {
-        const mockUser = {
-            _id: 'mockUserID',
-            isAuthorized: true,
-        };
-        jest.spyOn(User, 'findById').mockResolvedValueOnce(mockUser);
-
-        await adminController.getAdmin(req, res);
-
-        expect(User.findById).toHaveBeenCalledWith('mockUserID');
-        expect(res.render).toHaveBeenCalledWith('adminHome', {
-            layout: 'adminHome',
-            script: './js/admin.js',
+        beforeEach(() => {
+            req = {
+                session: {
+                    userID: 'mockUserID',
+                }
+            };
+            res = {
+                render: jest.fn(),
+                sendStatus: jest.fn(),
+            };
         });
-        expect(res.sendStatus).not.toHaveBeenCalled();
-    });
 
-    it('should send status 400 if user is unauthorized', async () => {
-        const mockUser = {
-            _id: 'mockUserID',
-            isAuthorized: false,
-        };
-        jest.spyOn(User, 'findById').mockResolvedValueOnce(mockUser);
+        afterEach(() => {
+            jest.clearAllMocks();
+        });
 
-        await adminController.getAdmin(req, res);
+        it('should render adminHome if user is authorized', async () => {
+            const mockUser = {
+                _id: 'mockUserID',
+                isAuthorized: true,
+            };
+            jest.spyOn(User, 'findById').mockResolvedValueOnce(mockUser);
 
-        expect(User.findById).toHaveBeenCalledWith('mockUserID');
-        expect(res.sendStatus).toHaveBeenCalledWith(400);
-        expect(res.render).not.toHaveBeenCalled();
-    });
+            await adminController.getAdmin(req, res);
 
-    it('should send status 400 if session.userID is undefined', async () => {
-        req.session.userID = undefined;
+            expect(User.findById).toHaveBeenCalledWith('mockUserID');
+            expect(res.render).toHaveBeenCalledWith('adminHome', {
+                layout: 'adminHome',
+                script: './js/admin.js',
+            });
+            expect(res.sendStatus).not.toHaveBeenCalled();
+        });
 
-        await adminController.getAdmin(req, res);
+        it('should send status 400 if user is unauthorized', async () => {
+            const mockUser = {
+                _id: 'mockUserID',
+                isAuthorized: false,
+            };
+            jest.spyOn(User, 'findById').mockResolvedValueOnce(mockUser);
 
-        expect(User.findById).not.toHaveBeenCalled();
-        expect(res.sendStatus).toHaveBeenCalledWith(400);
-        expect(res.render).not.toHaveBeenCalled();
-    });
+            await adminController.getAdmin(req, res);
 
-    it('should send status 400 if an error occurs', async () => {
-        jest.spyOn(User, 'findById').mockRejectedValueOnce(new Error('Database error'));
+            expect(User.findById).toHaveBeenCalledWith('mockUserID');
+            expect(res.sendStatus).toHaveBeenCalledWith(400);
+            expect(res.render).not.toHaveBeenCalled();
+        });
 
-        await adminController.getAdmin(req, res);
+        it('should send status 400 if session.userID is undefined', async () => {
+            req.session.userID = undefined;
 
-        expect(User.findById).toHaveBeenCalledWith('mockUserID');
-        expect(res.sendStatus).toHaveBeenCalledWith(400);
-        expect(res.render).not.toHaveBeenCalled();
+            await adminController.getAdmin(req, res);
+
+            expect(User.findById).not.toHaveBeenCalled();
+            expect(res.sendStatus).toHaveBeenCalledWith(400);
+            expect(res.render).not.toHaveBeenCalled();
+        });
+
+        it('should send status 400 if an error occurs', async () => {
+            jest.spyOn(User, 'findById').mockRejectedValueOnce(new Error('Database error'));
+
+            await adminController.getAdmin(req, res);
+
+            expect(User.findById).toHaveBeenCalledWith('mockUserID');
+            expect(res.sendStatus).toHaveBeenCalledWith(400);
+            expect(res.render).not.toHaveBeenCalled();
+        });
     });
 });

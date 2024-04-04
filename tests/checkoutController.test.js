3d08320f40cae1d4db5791d2d0b53f29dabafe51
katrombs <105538604+katrombs@users.noreply.github.com>
2024-04-04 19:11:19 +0800

    checkout and postCheckout finished, in progress checkoutSuccess

--- tests/checkoutController.test.js
+++ tests/checkoutController.test.js
@@ -6,6 +6,8 @@ import { Order } from '../model/orderSchema.js';
 
 import {jest} from '@jest/globals'
 import { beforeEach, describe, it } from 'node:test';
+import {Types} from 'mongoose';
+import fetch from 'node-fetch';
 
 jest.useFakeTimers()
 
@@ -13,6 +15,8 @@ jest.useFakeTimers()
 jest.mock('../model/userSchema.js');
 jest.mock('../model/productSchema.js');
 jest.mock('../model/orderSchema.js');
+// jest.mock('node-fetch');
+global.fetch = jest.fn();
 
 describe('Checkout controller', () => {
 
@@ -62,16 +66,61 @@ describe('Checkout controller', () => {
     describe('postCheckout function', () => {
 
         // successfully paid, return status 200
-        test('successful checkout, return status 200', async () => {
+        test('successful checkout', async () => {
             let req, res;
 
-            req = {};
+            req = {
+                body: {
+                    items: ['Brush', 'Gloves'],
+                    amount: [20, 10]
+                },
+                session: {
+                    userID: 'userID'
+                }
+            };
             res = {
-                render: jest.fn(),
-                sendStatus: jest.fn()
+                render: jest.fn(() => res),
+                sendStatus: jest.fn(() => res)
             };
 
-            
+            const user = {
+                _id: 'userID',
+                firstName: 'Peter',
+                lastName: 'Parker',
+                email: 'spiderman@marvel.com'
+            };
+            const prod1 = {
+                _id: 'prod1',
+                name: 'Brush',
+                price: 20,
+                productpic: 'prod1.jpg'
+            };
+            const prod2 = {
+                _id: 'prod2',
+                name: 'Gloves',
+                price: 10,
+                productpic: 'prod2.jpg'
+            };
+            const order = { _id: 'orderID' };
+
+            jest.spyOn(User, 'findById').mockReturnValue({ exec: jest.fn().mockResolvedValue(user) });
+
+            const testUser = await User.findById(user._id).exec();
+
+            const findByIdProductMock = jest.spyOn(Product, 'findById');
+            findByIdProductMock.mockResolvedValueOnce(prod1).mockResolvedValueOnce(prod2);
+
+            const saveMock = jest.spyOn(Order.prototype, 'save');
+            saveMock.mockResolvedValue(order);
+
+            const findByIdAndUpdateMock = jest.spyOn(Order, 'findByIdAndUpdate');
+            findByIdAndUpdateMock.mockResolvedValue(order);
+
+            await checkoutController.postCheckout(req,res);
+
+            expect(testUser).toEqual(user);
+            expect(User.findById).toHaveBeenCalledWith(user._id);
+            // expect(res.status).toHaveBeenCalledWith(200);
 
             jest.clearAllMocks();
         });
@@ -80,13 +129,40 @@ describe('Checkout controller', () => {
         test('entered an invalid amount, return status 401', async () => {
             let req, res;
 
-            req = {};
+            req = {
+                body: {
+                    _id: ['prod1', 'prod2'],
+                    items: ['Brush', 'Gloves'],
+                    amount: [1, 1]
+                },
+                session: {
+                    userID: 'userID'
+                } 
+            };
             res = {
                 render: jest.fn(),
                 sendStatus: jest.fn()
             };
 
+            const prod1 = {
+                _id: 'prod1',
+                name: 'Brush',
+                price: 1,
+                productpic: 'prod1.jpg'
+            };
+            const prod2 = {
+                _id: 'prod2',
+                name: 'Gloves',
+                price: 1,
+                productpic: 'prod2.jpg'
+            };
 
+            const findByIdProductMock = jest.spyOn(Product, 'findById');
+            findByIdProductMock.mockResolvedValueOnce(prod1).mockResolvedValueOnce(prod2);
+
+            await checkoutController.postCheckout(req,res);
+
+            expect(res.sendStatus).toHaveBeenCalledWith(401);
 
             jest.clearAllMocks();
         });
@@ -95,13 +171,43 @@ describe('Checkout controller', () => {
         test('failed to place order, return status 500', async () => {
             let req, res;
 
-            req = {};
+            req = {
+                body: {
+                    items: ['Brush', 'Gloves'],
+                    amount: [20, 10]
+                },
+                session: {
+                    userID: 'userID'
+                }
+            };
             res = {
-                render: jest.fn(),
-                sendStatus: jest.fn()
+                render: jest.fn(() => res),
+                sendStatus: jest.fn(() => res)
             };
 
+            const prod1 = {
+                _id: 'prod1',
+                name: 'Brush',
+                price: 1,
+                productpic: 'prod1.jpg'
+            };
+            const prod2 = {
+                _id: 'prod2',
+                name: 'Gloves',
+                price: 1,
+                productpic: 'prod2.jpg'
+            };
 
+            const findByIdProductMock = jest.spyOn(Product, 'findById');
+            findByIdProductMock.mockResolvedValueOnce(prod1).mockResolvedValueOnce(prod2);
+
+            const mockError = new Error('Placing order failed!');
+            jest.spyOn(console, 'error').mockImplementation(() => {});
+            jest.spyOn(Order.prototype, 'save').mockRejectedValueOnce(mockError);
+
+            await checkoutController.postCheckout(req, res);
+
+            expect(res.sendStatus).toHaveBeenCalledWith(500);
 
             jest.clearAllMocks();
         });
@@ -116,30 +222,83 @@ describe('Checkout controller', () => {
 
             req = {
                 params: {
-                    orderID: 123
+                    orderID: new Types.ObjectId()
+                },
+                session: {
+                    userID: '456'
                 }
             };
             res = {
                 render: jest.fn(),
                 sendStatus: jest.fn()
             };
+            const mockOrder = { 
+                _id: new Types.ObjectId(),
+                paymongoID: 'paymongo123' 
+            };
 
+            global.fetch = jest.fn().mockResolvedValue({
+                json: () => Promise.resolve({
+                    data: {
+                        attributes: {
+                            payment_intent: {
+                                attributes: {
+                                    status: 'succeeded'
+                                }
+                            },
+                            billing: {
+                                address: {
+                                    line1: '123 Street',
+                                    line2: 'Apt 101',
+                                    postal_code: '12345',
+                                    city: 'City',
+                                    state: 'State',
+                                    country: 'Country'
+                                },
+                                email: 'test@example.com'
+                            },
+                            payments: [{
+                                id: 'paymentId123'
+                            }]
+                        }
+                    }
+                })
+            });
 
+            // await checkoutController.checkoutSuccess(req,res);
+
+            // expect(Order.findById).toHaveBeenCalledWith(expect.any(ObjectId));
+            // expect(fetch).toHaveBeenCalledWith('https://api.paymongo.com/v1/checkout_sessions/paymongoId123', expect.any(Object));
+            // expect(Order.findByIdAndUpdate).toHaveBeenCalledWith(expect.any(ObjectId), expect.any(Object));
+            // expect(User.findByIdAndUpdate).toHaveBeenCalledWith('456', { cart: [] });
+            // expect(res.render).toHaveBeenCalledWith('checkoutSuccess', { orderID: '123' });
+            // expect(res.sendStatus).not.toHaveBeenCalled();
 
             jest.clearAllMocks();
-        });
+        }, 10000);
 
         // failed to fetch order, status 500
         test('failed to fetch order, return status 500', async () => {
             let req, res;
 
-            req = {};
+            req = { 
+                params: {
+                    orderID: new Types.ObjectId()
+                },
+                session: {
+                    userID: '456'
+                }
+            };
             res = {
                 render: jest.fn(),
                 sendStatus: jest.fn()
             };
+            
+            global.fetch.mockRejectedValueOnce(new Error('Failed to fetch order'));
 
+            await checkoutController.checkoutSuccess(req,res);
 
+            expect(res.sendStatus).toHaveBeenCalledWith(500);
 
             jest.clearAllMocks();
         });
@@ -162,5 +321,6 @@ describe('Checkout controller', () => {
         });
 
     });
+
 
 });
